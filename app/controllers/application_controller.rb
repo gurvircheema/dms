3fbaa8c2c5153d9b0958e3418a578f21b651c820
Gurvir Cheema <gurvircheema@hotmail.com>
2018-09-22 17:21:06 -0700

    Add new sign-in-form & layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
   before_action :authenticate_user!
 
   private
 
   def after_sign_in_path_for(user)
     root_path
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      'devise'
+    else
+      'application'
+    end
   end
 end
