753e0dbecaba84dd7065123af19a25258e052407
Gurvir Cheema <gurvircheema@hotmail.com>
2018-10-23 10:01:58 -0700

    Add Company seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do
+  Address.create!(
+    address_line_1: ENV['address_address_line_1'],
+    city: ENV['address_city'],
+    state_province: ENV['address_state_province'],
+    country: ENV['address_country'],
+    zipcode: ENV['address_zipcode'],
+  )
+
+  Company.create!(
+    name: ENV['company_name'],
+    address_id: Address.last.id,
+    contact: ENV['company_contact'],
+    phone: ENV['company_phone'],
+    starting_date: Date.new(2011, 6, 1),
+    fax: ENV['company_fax'],
+    email: ENV['company_email'],
+    website: ENV['company_website'],
+    state: ENV['company_state'],
+    currency: ENV['company_currency'],
+    last_invoice: ENV['company_last_invoice'],
+  )
+  byebug
+  false
+end
