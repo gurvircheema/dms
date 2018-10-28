29d72ffee5fab0578ade427717a0197df8aacc2e
Gurvir Cheema <gurvircheema@hotmail.com>
2018-10-28 11:50:42 -0700

    Add Load policy

---
+++ app/policies/load_policy.rb
@@ -0,0 +1,37 @@
+class LoadPolicy < ApplicationPolicy
+	attr_reader :user, :load
+
+	def initialize(user, load)
+		raise Pundit::NotAuthorizedError, "must be logged in" unless user
+		@user = user
+		@load = load
+	end
+
+	def index?
+		user.admin? || user.role?(:admin) || user.role?(:dispatcher)
+	end
+
+	def show?
+		index?
+	end
+
+	def new?
+		index?
+	end
+
+	def create?
+		index?
+	end
+
+	def edit?
+		index?
+	end
+
+	def update?
+		index?
+	end
+
+	def destroy?
+		index?
+	end
+end
---
+++ spec/support/roles_helper.rb
@@ -0,0 +1,10 @@
+module RolesHelper
+	def assign_role!(user, role)
+		UserRole.where(user: user).delete_all
+		UserRole.create(user: user, role: role)
+	end
+end
+
+RSpec.configure do |c|
+  c.include RolesHelper
+end
