7767817b871b63941a8ba0965799d34d22029cfc
Gurvir Cheema <gurvircheema@hotmail.com>
2018-11-18 18:27:22 -0800

    Add SES SMTP settings for production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,15 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: ENV['SES_SERVER_NAME'],
+    port: 587,
+    user_name: ENV["SES_SMTP_USERNAME"],
+    password: ENV["SES_SMTP_PASSWORD"],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
